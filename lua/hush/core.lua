de3457fae3c43cab27f318f147be29b6f7f64808
Maksym <bienkowski.maksym@gmail.com>
2025-05-31 19:23:13 +0200

    feat: add hush_all

--- lua/hush/core.lua
+++ lua/hush/core.lua
@@ -22,4 +22,22 @@ M.hush = function()
   end
 end
 
+M.hush_all = function()
+  local diagnostics = utils.get_diagnostic_map_for_current_line()
+
+  for source, codes in pairs(diagnostics) do
+    if sources[source] == nil then
+      vim.notify("Diagnostic source '" .. source .. "' is not supported", vim.log.levels.WARN, { title = "hush" })
+      goto continue
+    end
+
+    local comment = sources[source].build_suppress_all_diagnostics(codes)
+    local position = sources[source].comment_position
+    local gap_width = 2 -- TODO: parametrize
+    fileops.add_comment(comment, gap_width, position)
+
+    ::continue::
+  end
+end
+
 return M
--- lua/hush/init.lua
+++ lua/hush/init.lua
@@ -4,7 +4,7 @@ local M = {}
 
 M.setup = function()
   vim.api.nvim_create_user_command("Hush", hush.hush, {})
-  vim.api.nvim_create_user_command("HushAll", hush.hush, {})
+  vim.api.nvim_create_user_command("HushAll", hush.hush_all, {})
 end
 
 return M
